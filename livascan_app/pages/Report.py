546be9b5320aeced27bc758decb107d73d1dcaed
jaganfafs <jaganfafs@gmail.com>
2025-11-22 02:20:14 +0530

    Update Report.py

--- livascan_app/pages/Report.py
+++ livascan_app/pages/Report.py
@@ -6,7 +6,7 @@ st.set_page_config(page_title="Patient Report", page_icon="📄", layout="wide")
 with open("style.css") as css:
     st.markdown(f"<style>{css.read()}</style>", unsafe_allow_html=True)
 
-st.image("assets/report.png", use_column_width=True)
+st.image("livascan_app/assets/report.png", use_column_width=True)
 
 st.markdown("<h2 class='section-title'>Patient Report</h2>", unsafe_allow_html=True)
 
@@ -26,4 +26,5 @@ ax.pie([1], labels=[labels[result]], autopct='%1.1f%%')
 st.pyplot(fig)
 
 st.info("PDF report feature will be added soon.")
+
 
